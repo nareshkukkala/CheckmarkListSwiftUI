bb791030d281fd39579a4301c0a04b424118629e
Naresh Kukkala <nareshkukkala28@gmail.com>
2022-05-03 16:39:45 +0530

    Initial Push

---
+++ CheckmarkListSwiftUI/CheckmarkListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckmarkListSwiftUIApp.swift
+//  CheckmarkListSwiftUI
+//
+//  Created by naresh kukkala on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckmarkListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
